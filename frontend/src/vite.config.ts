7b7489ad8c0d50c21d4c364006f62d5406fe95dc
Kalil Pereira <kalil.pereira@fatec.sp.gov.br>
2025-11-04 08:40:15 -0300

    fix deploy

--- frontend/src/vite.config.ts
+++ frontend/src/vite.config.ts
@@ -1,10 +1,13 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Importe o 'path'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Diga ao Vite que seu código-fonte está na pasta 'src'
+      root: path.resolve(__dirname, 'src'),
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -16,8 +19,14 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          // Ajuste o alias para funcionar a partir do novo root 'src'
+          '@': path.resolve(__dirname, 'src'),
         }
+      },
+      build: {
+        // Diga ao Vite para colocar o build na pasta 'dist'
+        // no nível de 'frontend/', não 'frontend/src/dist'
+        outDir: path.resolve(__dirname, 'dist')
       }
     };
 });
